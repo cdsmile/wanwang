3e875c34bed1c956ef49318909cdd9f348afe00d
cdsmile <cdcdlove@126.com>
2012-11-05 14:34:41 +0800

    0.0
    0.0

---
+++ wanwang/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  wanwang
+//
+//  Created by Ibokan on 12-11-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
